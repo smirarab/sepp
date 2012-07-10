13895cfaa0309ec4a0b051483f266b3e3e767150
Siavash <smirarab@gmail.com>
2012-07-10 08:46:59 -0500

    misc.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 ###########################################################################
-##    Copyright 2010 Rahul Suri and Tandy Warnow.
-##    This file is part of ReUP.
+##    Copyright 2012 Siavash Mirarab, Nam Nguyen, and Tandy Warnow.
+##    This file is part of SEPP.
 ##
-##    ReUP is free software: you can redistribute it and/or modify
+##    SEPP is free software: you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
 ##    the Free Software Foundation, either version 3 of the License, or
 ##    (at your option) any later version.
 ##
-##    ReUP is distributed in the hope that it will be useful,
+##    SEPP is distributed in the hope that it will be useful,
 ##    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ##    GNU General Public License for more details.
 ##
 ##    You should have received a copy of the GNU General Public License
-##    along with ReUP.  If not, see <http://www.gnu.org/licenses/>.
+##    along with SEPP.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
 #from distutils.core import setup
@@ -32,7 +32,7 @@ setup(name = "sepp",
       author_email = "smirarab@gmail.com, namphuon@cs.utexas.edu",
 
       license="General Public License (GPL)",
-      requires = ["dendropy (>= 3.4)", "bio (>= 1.0)"],
+      install_requires = ["dendropy >= 3.4", "biopython >= 1.0"],
       provides = ["sepp"],
       scripts = ["sepp/scripts/run_sepp.py"],
 
@@ -44,4 +44,3 @@ setup(name = "sepp",
                      "Operating System :: OS Independent",
                      "Programming Language :: Python",
                      "Topic :: Scientific/Engineering :: Bio-Informatics"])
-
