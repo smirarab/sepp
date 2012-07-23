8c22317eded8e93d0ebc8492b8c05f4453c2114a
Siavash Mirarab <smirarab@gmail.com>
2012-07-23 16:40:01 -0500

    Fixed banners

--- sepp/alignment.py
+++ sepp/alignment.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+###########################################################################
+##    Copyright 2012 Siavash Mirarab, Nam Nguyen, and Tandy Warnow.
+##    This file is part of SEPP, and is adopted from SATe software package 
+##   (https://github.com/sate-dev/sate-core/blob/master/sate/alignment.py).
+##
+##    SEPP is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    SEPP is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with SEPP.  If not, see <http://www.gnu.org/licenses/>.
+###########################################################################
+
 """
 Simple classes for reading and manipulating sequence data matrices
 """
--- sepp/utilities.py
+++ sepp/utilities.py
@@ -1,8 +1,21 @@
-'''
-Created on Jun 7, 2011
+##########################################################################, a#
+##    Copyright 2012 Siavash Mirarab, Nam Nguyen, and Tandy Warnow.
+##    This file is part of SEPP.
+##
+##    SEPP is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    SEPP is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with SEPP.  If not, see <http://www.gnu.org/licenses/>.
+###########################################################################
 
-@author: smirarab
-'''
 from dendropy import treecalc
 from sepp import sortByValue
 from Bio import AlignIO
