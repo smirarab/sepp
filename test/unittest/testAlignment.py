93bdd31f810def05462622fe7999a2305ef87ed2
Siavash Mirarab <smirarab@gmail.com>
2012-11-16 13:05:09 -0600

    Adding more test cases

---
+++ test/unittest/testAlignment.py
@@ -0,0 +1,143 @@
+'''
+Created on Sep 19, 2012
+
+@author: smirarab
+'''
+import unittest
+import sepp
+import copy
+sepp._DEBUG = True
+from sepp.alignment import MutableAlignment, ReadonlySubalignment,\
+    ExtendedAlignment
+from sepp.problem import SeppProblem        
+
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+
+    def testAlignmentReadFasta(self):
+        print "====== starting testAlignmentReadFasta ==========" 
+        alg = MutableAlignment()
+        alg.read_filepath("data/mock/pyrg/sate.fasta")
+        
+        print "Maing alignment is:\n\n", alg
+        
+        assert len(alg) == 65, "MutableAlignment length is %s" %len(alg)
+        
+        assert all([not alg.is_all_gap(i) for i in xrange(0,alg.get_length())])            
+            
+
+    def testReadOnlySubAlignment(self):
+        print "======= starting testReadOnlySubAlignment =========" 
+        alg = MutableAlignment()
+        alg.read_filepath("data/mock/pyrg/sate.fasta")
+        
+        subset  = alg.keys()[9:12]
+        readonly_subalignment = ReadonlySubalignment(subset, alg)
+        
+        print "subalignment is:\n\n", readonly_subalignment
+        
+        assert len(readonly_subalignment) == 3, len(readonly_subalignment) 
+        
+        assert readonly_subalignment.keys() == readonly_subalignment.get_sequence_names() == subset, "Subalignment keys not matching given keys %s vs %s" %(readonly_subalignment.keys() , subset)
+        
+        for (k, s) in readonly_subalignment.items():
+            assert k in subset, "%s not found in subset but returned by subalignment" %k
+            assert s == alg[k], "sequence associated with %k not matching parent alignment" %k 
+        
+        try:
+            readonly_subalignment[2] = "ACGT"
+            assert False, "Readony alignment is successfully modified. "
+        except TypeError:
+            pass
+        
+        assert readonly_subalignment.get_length() == alg.get_length(), "alignment length should not change"
+        
+        assert readonly_subalignment.is_aligned() == True
+        
+        assert readonly_subalignment.is_all_gap(2) == True, "Site 2 should be all gaps"
+        assert readonly_subalignment.is_all_gap(150) == False, "Site 100 should not be all gaps"        
+        
+        readonly_subalignment.write_to_path("data/mock/pyrg/sate.sub.fasta")
+        
+        mutable_subalignment = readonly_subalignment.get_mutable_alignment()
+        mutable_subalignment.delete_all_gap()
+        
+        assert all([not mutable_subalignment.is_all_gap(i) for i in xrange(0,mutable_subalignment.get_length())])
+        
+        print "======= finishing testReadOnlySubAlignment =========" 
+
+    def testExtendedAlignment(self):
+        print "======= starting testExtendedAlignment ========="
+
+        subset = ["SFIF","SFII","SCFC","SGHD","SDCC","SBGE","SFBB","SDI","SCGB","SJGF","SGBI","SCJA","SGAD","SHEB","SFHB","SDJI","SHED","SJJJ","SBBE","SCCH","SDJB","SDAC","SHEH","SFDC","SFEI","SHHB","SC","SIAB","SDDI","SBCB","SJB","SEBD","SFGD","SHA","SIDA","SGHI","SGIB","SBFJ","SFIE","SCJF","SJHJ","SJBG","SEJI","SFFF","SJ","SIII","SJHH","SEIH","SBDC","SHDJ","SJDD","SGDB","SIHA","SIBB","SECC","SCAD","SGBB","SGIF","SJHC","SFCD","SEAA","SEFF","SDFG","SDJE","SCFG","SFH","SCJ","SDDD","SEGD","SCIH","SDAG","SCJE","SFAJ","SIDJ","SE","SHBC","SJFF","SCHD","SBHA","SEDF","SFAF","SEDD","SDHD","SGJD","SIBH","SGDF","SIFA","SJGA","SIJB","SFI","SGA","SBFC","SBJA","SFFC","SFDH","SFEE","SBDF","SGBJ","SDHE","SJIB","SHHI","SIDE","SJII"]
+         
+        alg = MutableAlignment()
+        alg.read_filepath("data/simulated/test.fasta")
+        alg.delete_all_gap()
+        tlen = alg.get_length()                    
+        
+        frg = MutableAlignment()
+        frg.read_filepath("data/simulated/test.fas")
+        #print frg.get_num_taxa()
+        
+        pp = SeppProblem(alg.keys())
+        pp.fragments = frg
+        pp.subalignment = alg
+        
+        cp1 = SeppProblem(subset, pp)
+        cp2 = SeppProblem(list(set(alg.keys()) -set(subset)), pp)
+        cp1.fragments = ReadonlySubalignment([k for k in frg.keys() if int(k[-1]) >= 9], frg)
+        cp2.fragments = ReadonlySubalignment([k for k in frg.keys() if int(k[-1]) <= 1], frg)
+        
+        cp1labels = cp1.write_subalignment_without_allgap_columns("data/tmp/cp1.fasta")
+        cp2labels = cp2.write_subalignment_without_allgap_columns("data/tmp/cp2.fasta")
+        tmp = MutableAlignment().read_filepath("data/tmp/cp1.fasta")
+        assert all([not tmp.is_all_gap(pos) for pos in xrange(0,tmp.get_length())])        
+        tmp = MutableAlignment().read_filepath("data/tmp/cp2.fasta")
+        assert all([not tmp.is_all_gap(pos) for pos in xrange(0,tmp.get_length())])
+        
+        cp1.fragments.write_to_path("data/tmp/cp1.frags.fas")
+        cp2.fragments.write_to_path("data/tmp/cp2.frags.fas")
+        
+        '''We have done the hmmalign before. don't worry about that right now'''
+        
+        ext1 = ExtendedAlignment(cp1.fragments)
+        ext1.build_extended_alignment("data/tmp/cp1.fasta", "data/tmp/cp1.extended.sto")
+        ext1.relabel_original_columns(cp1labels)
+        ext2 = ExtendedAlignment(cp2.fragments)
+        ext2.build_extended_alignment("data/tmp/cp2.fasta", "data/tmp/cp2.extended.sto")
+        ext2.relabel_original_columns(cp2labels)
+        
+        extmerger = ExtendedAlignment([])
+        extmerger.merge_in(ext1)
+        extmerger.merge_in(ext2)
+                        
+        extmerger.write_to_path("data/tmp/extended.merged.fasta")        
+
+        assert extmerger.is_aligned(), "Merged alignment is not aligned"
+        in1 = len([x for x in ext1._col_labels if x<0])
+        in2 = len([x for x in ext2._col_labels if x<0])
+        print "Merged:%d. Insertion1:%d Insertion2:%d BaseLen:%d" %(extmerger.get_length(),in1 , in2 , tlen)
+        assert ( in1 + in2 + tlen) == extmerger.get_length(), "Lengths don't match up after merging. Merged:%d. Insertion1:%d Insertion2:%d BaseLen:%d" %(extmerger.get_length(),in1 , in2 , tlen)
+         
+        
+        tmp = extmerger.get_base_readonly_alignment().get_mutable_alignment()
+        tmp.delete_all_gap()
+        assert tmp.is_aligned(), "merged alignment should be aligned!"
+        assert tmp.get_length() == tlen, "merged alignment has wrong length"
+        assert all([alg[k] == s for (k,s) in tmp.items()]), "merged alignment should match original alignment"
+
+        
+        print "======= finished testExtendedAlignment ========="
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testAlignmentReadFasta']
+    unittest.main()
---
+++ test/unittest/testExternalJobs.py
@@ -0,0 +1,116 @@
+'''
+Created on Oct 2, 2012
+
+@author: smirarab
+'''
+import unittest
+from sepp.jobs import ExternalSeppJob
+from sepp.scheduler import JobPool, JobError
+
+class TestExternalJob(ExternalSeppJob):
+
+    def __init__(self, pipe = 0, **kwargs):
+        self.pipe = pipe
+        if pipe == 0:
+            '''By default errors and output is piped to this process.
+            output/err will be available at as self.stdoutdata and self.stderrdata
+            and also will be logged'''
+            pass           
+        elif pipe == 1:
+            '''The following line forces output to be saved to a file. Note that
+            1- A file object/descriptor cannot be used here because this is 
+               executed in parent process, and in child process those file handlers
+               will not be available.  
+            2- self.stdoutdata and self.stderrdata will not be automatically 
+               filled (to avoid unnecessary I/O). Read stderr and stdout in
+               read_results if necessary. 
+            '''
+            kwargs['stdout'] = "./.stdout"
+            kwargs['stderr'] = "./.stderr"
+
+        
+        '''always call parent constructor'''
+        ExternalSeppJob.__init__(self, "test_find_job", **kwargs)
+
+        '''inputs that need to be set before running this job'''
+        self.pattern = None
+        self.options = None
+    
+    def get_invocation(self):
+        return ['find', self.pattern] + ([] if self.options is None else self.options.split())
+
+    def characterize_input(self):
+        return "pattern: %s options: %s (%s)" %(self.pattern, self.options, "pipe: %d" %self.pipe)
+
+    def read_results(self):        
+        return open("./.stdout").read() if self.pipe == 2 else self.stdoutdata if self.stdoutdata is not None else "Nothing captured" 
+
+class Test(unittest.TestCase):
+
+    def testSuccess(self):            
+        find_job = TestExternalJob()
+        find_job.pattern = "."
+        find_job.options = "-name *.py"
+        JobPool().enqueue_job(find_job)
+        
+        JobPool().wait_for_all_jobs()                
+        
+        res0 = find_job.result.split('\n')[0]
+        
+        assert res0 != ""
+
+        find_job = TestExternalJob()
+        find_job.pattern = ".."
+        find_job.options="-name %s" %res0.replace("./test","*")
+        JobPool().enqueue_job(find_job)
+        
+        JobPool().wait_for_all_jobs()
+        
+        res1 = find_job.result.split('\n')[0]
+        
+        assert res1 != ""
+        
+        assert res1.replace("../unittest/","./") == res0
+
+
+    def testError(self):            
+        find_job = TestExternalJob()
+        find_job.pattern = "somerandomdirectorywewillneverhavehere_ordowe"
+        ''' Let's ignore this error in subsequent test cases. '''
+        find_job.ignore_error = True
+        try:
+            JobPool().enqueue_job(find_job)        
+            assert JobPool().wait_for_all_jobs() == False            
+        except JobError as e:
+            assert str(e).find("No such file or directory") != -1, "The error we expected is no such file or directory"                 
+        
+        assert JobPool().get_asynch_result_object(find_job).successful() == False, "We expected the job to fail"
+        
+
+        
+    def testNoPipe(self):
+        find_job = TestExternalJob(pipe=1)
+        find_job.pattern = "."
+        find_job.options = "-name *.py"
+        JobPool().enqueue_job(find_job)        
+        JobPool().wait_for_all_jobs()                        
+        res0 = find_job.result.split('\n')[0]                
+        assert res0 != ""
+        
+        find_job = TestExternalJob(pipe=1)
+        find_job.pattern = "somerandomdirectorywewillneverhavehere_ordowe"
+        ''' Let's ignore this error in subsequent test cases. '''
+        find_job.ignore_error = True
+        try:
+            JobPool().enqueue_job(find_job)        
+            assert JobPool().wait_for_all_jobs() == False
+            JobPool().get_asynch_result_object(find_job).get()
+        except JobError as e:
+            assert str(e).find("No such file or directory") != -1, "The error we expected is no such file or directory"                 
+        
+        assert JobPool().get_asynch_result_object(find_job).successful() == False, "We expected the job to fail"        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testBaseExternalJob']
+    unittest.main()
