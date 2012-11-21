b092b5ba274c18fecedd313b8054bb23672b306d
Siavash Mirarab <smirarab@gmail.com>
2012-11-21 15:51:49 -0600

    Adding math_utils.py

---
+++ sepp/math_utils.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 14, 2012
+
+@author: smirarab
+'''
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:      
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
