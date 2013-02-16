0319a8476429e7c6030bfe4f4610b37f4c859b36
bendmorris <ben@bendmorris.com>
2013-02-15 19:33:47 -0500

    Pull out scientific or common names from names.dmp; store them in two dictionaries.

--- ncbi_taxonomy.py
+++ ncbi_taxonomy.py
@@ -1,8 +1,11 @@
 import urllib2
-import Bio.Phylo as bp
+from Bio.Phylo import Newick
 import os
 import tarfile
 
+
+col_delimiter = '\t|\t'
+row_delimiter = '\t|\n'
 
 # download the taxonomy archive
 url = 'ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz'
@@ -25,3 +28,18 @@ for filename in ('nodes.dmp', 'names.dmp'):
         print 'Extracting %s...' % filename
         archive = tarfile.open(name=filename, mode='r:gz')
         archive.extract(filename)
+
+# get names for all tax_ids from names.dmp
+scientific_names = {}
+common_names = {}
+with open('names.dmp') as names_file:
+    for line in names_file:
+        line = line.rstrip(row_delimiter)
+        values = line.split(col_delimiter)
+        tax_id, name_txt, _, name_type = values[:4]
+        if name_type == 'scientific name':
+            scientific_names[tax_id] = name_txt
+        elif name_type == 'common name':
+            common_names[tax_id] = name_txt
+
+# create tree from nodes.dmp
