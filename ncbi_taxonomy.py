2d1ea74df2d0614f6876281d0a0063b41fc02700
bendmorris <ben@bendmorris.com>
2013-02-15 19:27:43 -0500

    Extract the two needed text dumps, names.dmp and nodes.dmp.

--- ncbi_taxonomy.py
+++ ncbi_taxonomy.py
@@ -1,6 +1,7 @@
 import urllib2
 import Bio.Phylo as bp
 import os
+import tarfile
 
 
 # download the taxonomy archive
@@ -9,9 +10,18 @@ filename = url.split('/')[-1]
 if os.path.exists(filename):
     print 'Using existing copy of %s' % filename
 else:
+    print 'Downloading %s...' % filename
     r = urllib2.urlopen(urllib2.Request(url))
     assert r.geturl() == url
     with open(filename, 'wb') as output_file:
         output_file.write(r.read())
     r.close()
 
+# extract the text dump
+for filename in ('nodes.dmp', 'names.dmp'):
+    if os.path.exists(filename):
+        print 'Using existing copy of %s' % filename
+    else:
+        print 'Extracting %s...' % filename
+        archive = tarfile.open(name=filename, mode='r:gz')
+        archive.extract(filename)
