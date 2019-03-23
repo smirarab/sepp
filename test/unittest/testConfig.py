eaa7d100637026c24a30cadd125f4b00d2d50be0
Stefan Janssen <stefan.m.janssen@gmail.com>
2019-03-23 09:15:56 +0100

    fixing syntax

--- test/unittest/testConfig.py
+++ test/unittest/testConfig.py
@@ -27,26 +27,26 @@ class Test(unittest.TestCase):
         sys.argv = [sys.argv[0], "-A", "2", "-c", "data/configs/test.config",
                     "--outdir", "dir_form_commandline"]
 
-        assert(options().alignment_size == 2,
-               "Commandline option -A not read properly")
+        assert options().alignment_size == 2, \
+            "Commandline option -A not read properly"
 
-        assert(isinstance(options().config_file, filetypes)
-               and options().config_file.name == "data/configs/test.config",
-               "Commandline option -c not read properly")
+        assert isinstance(options().config_file, filetypes) \
+            and options().config_file.name == "data/configs/test.config", \
+            "Commandline option -c not read properly"
 
-        assert((options().pplacer is not None
-               and options().pplacer.path == "pplacer"),
-               "config file options not read properly")
+        assert (options().pplacer is not None
+                and options().pplacer.path == "pplacer"), \
+            "config file options not read properly"
 
-        assert(options().placement_size == 10,
-               "Config file option placementSize not read properly")
+        assert options().placement_size == 10, \
+            "Config file option placementSize not read properly"
 
-        assert(options().outdir.endswith("dir_form_commandline"),
-               "Config file value outdir is not properly overwritten:%s " %
-               options().outdir)
+        assert options().outdir.endswith("dir_form_commandline"), \
+            "Config file value outdir is not properly overwritten:%s " % \
+            options().outdir
 
-        assert(options().tempdir is not None,
-               "Default value not properly set for tempfile attribute")
+        assert options().tempdir is not None, \
+            "Default value not properly set for tempfile attribute"
 
         config.main_config_path = back
 
@@ -60,18 +60,18 @@ class Test(unittest.TestCase):
 
         sys.argv = [sys.argv[0], "-c", "data/configs/test2.config", "-f",
                     "data/simulated/test.fas"]
-        assert(isinstance(options().config_file, filetypes)
-               and options().config_file.name == "data/configs/test2.config",
-               "Commandline option -c not read properly")
+        assert isinstance(options().config_file, filetypes) \
+            and options().config_file.name == "data/configs/test2.config", \
+            "Commandline option -c not read properly"
 
-        assert(isinstance(options().alignment_file, filetypes)
-               and options().alignment_file.name ==
-               "data/simulated/test.small.fas",
-               "Config file option alignment not read properly")
+        assert isinstance(options().alignment_file, filetypes) \
+            and options().alignment_file.name == \
+            "data/simulated/test.small.fas", \
+            "Config file option alignment not read properly"
 
-        assert(isinstance(options().fragment_file, filetypes)
-               and options().fragment_file.name == "data/simulated/test.fas",
-               "Command-line option -f alignment not read properly")
+        assert isinstance(options().fragment_file, filetypes) \
+            and options().fragment_file.name == "data/simulated/test.fas", \
+            "Command-line option -f alignment not read properly"
 
         config.main_config_path = back
 
@@ -81,21 +81,21 @@ class Test(unittest.TestCase):
 
         sys.argv = [sys.argv[0]]
 
-        assert((options().pplacer is not None and os.path.exists(
-               options().pplacer.path)),
-               ("main config file options not read properly,"
-                " or nonexistent binaries: pplacer = %s" %
-                options().pplacer.path))
+        assert (options().pplacer is not None and os.path.exists(
+                options().pplacer.path)), \
+            ("main config file options not read properly,"
+             "or nonexistent binaries: pplacer = %s" %
+             options().pplacer.path)
 
-        assert((options().hmmalign is not None and os.path.exists(
-               options().hmmalign.path)),
-               ("main config file options not read properly, or nonexistent "
-                "binaries: hmmalign = %s" % options().pplacer.path))
+        assert (options().hmmalign is not None and os.path.exists(
+                options().hmmalign.path)), \
+            ("main config file options not read properly, or nonexistent "
+             "binaries: hmmalign = %s" % options().pplacer.path)
 
-        assert((options().hmmsearch is not None and os.path.exists(
-               options().hmmalign.path)),
-               ("main config file options not read properly, or nonexistent "
-                "binaries: hmmsearch = %s" % options().pplacer.path))
+        assert (options().hmmsearch is not None and os.path.exists(
+                options().hmmalign.path)), \
+            ("main config file options not read properly, or nonexistent "
+             "binaries: hmmsearch = %s" % options().pplacer.path)
 
     def testCpuCount(self):
         # Just to make different test cases independent of each other.
